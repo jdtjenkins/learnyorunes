3f2b118cfb6f1d5f4de39bb97cb32cec31e41435
Jacob Jenkins <jacob_jenkins@live.com>
2025-10-26 23:12:16 +0000

    initial commit

---
+++ src/components/consts.ts
@@ -0,0 +1,109 @@
+export const runes = {
+    aesh: {
+        imgUrl: `https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/c/c5/Aesh.png/84px-Aesh.png`,
+        description: "The Staff, Rune of Thought",
+    },
+    bravash: {
+        imgUrl: "https://profounddecisions.co.uk/mediawiki-public/images/thumb/e/ec/Bravash.png/91px-Bravash.png",
+        description: "The Seedpod, Rune of Fertility",
+    },
+    cavul: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/f/f6/Cavul.png/75px-Cavul.png",
+        description: "The Swan, Rune of Purity",
+    },
+    diras: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/f/fe/Diras.png/75px-Diras.png",
+        description: "The Shuttered Lantern, Rune of Secrets",
+    },
+    evrom: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/d/dc/Evrom.png",
+        description: "The Sapling, The Rune of Beginning",
+    },
+    feresh: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/a/a2/Feresh.png/187px-Feresh.png",
+        description: "The Eagle, Rune of Majesty",
+    },
+    gralm: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/6/66/Gralm.png",
+        description: "The Crossroads, Rune of Destiny",
+    },
+    hirmok: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/b/b6/Hirmok.png/187px-Hirmok.png",
+        description: "The Scythe, Rune of Dominion",
+    },
+    irremais: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/7/7a/Irremais.png",
+        description: "The Blade, Rune of Wisdom",
+    },
+
+    
+    jotra: {
+        imgUrl: `https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/c/c2/Jotra.png/187px-Jotra.png`,
+        description: "The Swords, Rune of Battle",
+    },
+    kyrop: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/4/48/Kyrop.png",
+        description: "The Fish, Rune of Weakness",
+    },
+    lann: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/a/ad/Lann.png/187px-Lann.png",
+        description: "The Hound, Rune of Bargains",
+    },
+    mawrig: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/7/70/Mawrig.png",
+        description: "The Torrent, Rune of Storms and Disaster",
+    },
+    naeve: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/4/47/Naeve.png",
+        description: "The Fang, Rune of Hunger",
+    },
+    ophis: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/6/69/Ophis.png/187px-Ophis.png",
+        description: "The Bright Lantern, Rune of Revelation",
+    },
+    pallas: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/f/fb/Pallas.png/187px-Pallas.png",
+        description: "The Apple Tree, Rune of Wealth",
+    },
+    queros: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/b/bf/Queros.png",
+        description: "The Wave, Rune of Plots",
+    },
+    rhyv: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/3/31/Rhyv.png",
+        description: "The Bleeding Wound, Rune of Blood",
+    },
+
+    sular: {
+        imgUrl: `https://www.profounddecisions.co.uk/mediawiki-public/images/7/75/Sular.png`,
+        description: "The Ship, Rune of Discovery",
+    },
+    tykonus: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/4/4a/Tykonus.png/187px-Tykonus.png",
+        description: "The Banner, Rune of Victory",
+    },
+    ull: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/9/99/Ull.png",
+        description: "The Road, Rune of Chance",
+    },
+    verys: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/1/14/Verys.png",
+        description: "The Lion, Rune of Might",
+    },
+    wyr: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/6/68/Wyr.png/187px-Wyr.png",
+        description: "The Serpent, Rune of Mystery",
+    },
+    xun: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/d/d6/Xun.png/187px-Xun.png",
+        description: "The Wyrm, Rune of Transformation",
+    },
+    yoorn: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/f/f8/Yoorn.png",
+        description: "The Shears, Rune of Ending",
+    },
+    zorech: {
+        imgUrl: "https://www.profounddecisions.co.uk/mediawiki-public/images/thumb/f/f6/Zorech.png/187px-Zorech.png",
+        description: "The Flame, Rune of Passion",
+    },
+}
---
+++ src/components/learn.tsx
@@ -0,0 +1,70 @@
+import { createMemo, createSignal, Show } from "solid-js"
+import { runes } from './consts';
+
+export function Learn() {
+    const [rune, setRune] = createSignal<string | null>(null)
+    const currentRune = createMemo(() => ({
+        rune: rune(),
+        ...runes[rune() as keyof typeof runes],
+    }))
+    const [ correct, setCorrect ] = createSignal<boolean | null>(null)
+
+    const setRandomRune = () => {
+        const runeNames = Object.keys(runes)
+        const randomRune = runeNames[Math.floor(Math.random() * runeNames.length)]
+
+        setRune(randomRune)
+        setCorrect(null)
+    }
+
+    const onSubmit = (value: string) => {
+        console.log("awdawd")
+        if (value.toLowerCase() === rune()?.toLowerCase()) {
+            setCorrect(true)
+        } else {
+            setCorrect(false)
+        }
+    }
+
+    setRandomRune()
+
+    return (
+        <>
+            <div class="fixed top-0 left-0 w-full h-full p-4 grid grid-rows-[auto_1fr] gap-8">
+                <h1 class="font-title text-2xl text-center font-bold">Who's that runémon?</h1>
+                <div class="w-full h-full flex items-center justify-center flex-col gap-4">
+                    <img src={currentRune().imgUrl} alt={currentRune().rune!} />
+                    <p>{ currentRune().description }</p>
+                    <label class="input">
+                        <span>Rune name:</span>
+                        <input
+                            type="text"
+                            class="grow"
+                            on:change={event => {
+                                event.preventDefault()
+                                event.stopPropagation()
+
+                                onSubmit(event.currentTarget.value)
+                            }}
+                        />
+                    </label>
+                    <Show when={correct() === false}>
+                        <p class="alert alert-error">Incorrect!</p>
+                    </Show>
+                    <Show when={correct() === true}>
+                        <p class="alert alert-success alert-soft">Well done!</p>
+                        <button
+                            class="btn btn-success btn-lg"
+                            on:click={() => setRandomRune()}
+                        >Another!</button>
+                    </Show>
+                </div>
+            </div>
+            <a
+                href="https://www.profounddecisions.co.uk/empire-wiki/Runes"
+                target="_blank"
+                class="fixed bottom-4 left-4 font-light text-sm text-base-content/80"
+            >For the cheaters</a>
+        </>
+    )
+}
